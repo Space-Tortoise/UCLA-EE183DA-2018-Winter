2869c3d157ef762244cd97dd360c928b0741533c
Space-Tortoise <35677144+Space-Tortoise@users.noreply.github.com>
2018-02-02 14:13:10 -0800

    Add files via upload

---
+++ Lab2/servotest2.ino
@@ -0,0 +1,135 @@
+#include <Servo.h>
+Servo servo1;
+void setup() {
+servo1.attach(D4);
+}
+
+void loop() {
+  servo1.write(30);
+delay(2000);
+
+servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(30);   // Tell servo to go to 180 degrees
+
+  delay(280);   
+
+  
+  servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(30);   // Tell servo to go to 180 degrees
+
+  delay(280);   
+
+  
+  servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(40);   // Tell servo to go to 180 degrees
+
+  delay(210);   
+
+
+    servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(210);         // Pause to get it time to move
+
+  servo1.write(50);   // Tell servo to go to 180 degrees
+
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+    servo1.write(30);   // Tell servo to go to 180 degrees
+
+  delay(280);   
+
+
+  
+  servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+    servo1.write(40);    // Tell servo to go to 90 degrees
+
+  delay(210);         // Pause to get it time to move
+
+
+
+   servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(210);         // Pause to get it time to move
+
+  servo1.write(50);   // Tell servo to go to 180 degrees
+
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+    servo1.write(30);   // Tell servo to go to 180 degrees
+    delay(280);         // Pause to get it time to move
+
+      servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(50);   // Tell servo to go to 180 degrees
+
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+    servo1.write(45);   // Tell servo to go to 180 degrees
+    delay(280);         // Pause to get it time to move
+    delay(500);
+
+for(int i=0; i<4;i++)
+{
+    servo1.write(70);    // Tell servo to go to 90 degrees
+  delay(203);         // Pause to get it time to move
+  servo1.write(50);   // Tell servo to go to 180 degrees
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+   servo1.write(50);   // Tell servo to go to 180 degrees
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+    servo1.write(45);   // Tell servo to go to 180 degrees
+    delay(203);         // Pause to get it time to move
+}
+
+
+  servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(203);         // Pause to get it time to move
+    servo1.write(30);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+
+
+  for(int i=0; i<2;i++)
+{
+
+   servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(50);   // Tell servo to go to 180 degrees
+
+  delay(140);   
+   servo1.write(70);
+   delay(140);   
+    servo1.write(30);   // Tell servo to go to 180 degrees
+    delay(280);         // Pause to get it time to move
+
+}
+
+    delay(1000);
+
+
+    
+
+}
