ad752a0f721363447c493748acb9d12733ff126d
Space-Tortoise <35677144+Space-Tortoise@users.noreply.github.com>
2018-02-02 14:12:46 -0800

    Add files via upload

---
+++ Lab2/servotest.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+Servo servo1;
+void setup() {
+servo1.attach(D4);
+}
+
+void loop() {
+for(int i=0;i<7;i++)
+{
+servo1.write(70);    // Tell servo to go to 90 degrees
+
+  delay(280);         // Pause to get it time to move
+
+  servo1.write(30);   // Tell servo to go to 180 degrees
+
+  delay(350);   
+}
+delay(500);
+}
