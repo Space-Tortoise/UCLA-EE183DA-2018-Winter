703a40e2ec0817657c81be44d877a41b52d20713
Space-Tortoise <35677144+Space-Tortoise@users.noreply.github.com>
2018-02-02 21:28:20 -0800

    Update servotest.ino

--- Lab2/servotest.ino
+++ Lab2/servotest.ino
@@ -1,5 +1,8 @@
 #include <Servo.h>
 Servo servo1;
+int k=0;
+int p=7*k;
+
 void setup() {
 servo1.attach(D4);
 }
@@ -9,11 +12,11 @@ for(int i=0;i<7;i++)
 {
 servo1.write(70);    // Tell servo to go to 90 degrees
 
-  delay(280);         // Pause to get it time to move
+  delay(280-p);         // Pause to get it time to move
 
-  servo1.write(30);   // Tell servo to go to 180 degrees
+  servo1.write(30+k);   // Tell servo to go to 180 degrees
 
-  delay(350);   
+  delay(350-p);   
 }
-delay(500);
+delay(500-p);
 }
