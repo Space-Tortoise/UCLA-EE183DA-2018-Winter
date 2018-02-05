52efb54eeb3219b51b709b1323972f2a69e96a25
Space-Tortoise <35677144+Space-Tortoise@users.noreply.github.com>
2018-02-04 20:16:05 -0800

    Update NEW_row_row_row_your_boat .ino

--- Lab2/NEW_row_row_row_your_boat .ino
+++ Lab2/NEW_row_row_row_your_boat .ino
@@ -1,7 +1,8 @@
 #include <Servo.h>
 Servo servo1;
 
-int k=0; // K is better between -30 and 10, any number that is outside this boundary might not work so well.
+int k=0; // K is the only parameter that you need to change to get the different tempos for both the servo and the buzzer.
+         // K is better between -30 and 10, any number that is outside this boundary might not work so well.
          // The positive numbers will make it faster and the negative numbers will make it slower.
 
 
@@ -19,215 +20,204 @@ pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  servo1.write(30+k);
-delay(2000);
+   servo1.write(30+k); //let the servo go to the starting position.
+   delay(2000); // Wait for 2 seconds and start playing.
 
          
-servo1.write(70);    
-  delay(280-p); //by the end of this delay, the servo will reach 70 degree and hit the "drum" .
-   tone(buzzerPin, frequency('c'), duration_L-p);  //At the same moment, the buzzer will play a note, 
-                                                   //this is how they get synchronized.   
-
-  servo1.write(30+k);   
-
-  delay(280-p);   
+   servo1.write(70); //The "drum" is fixed at 70 degree in this case.   
+   delay(280-p); //By the end of this delay, the servo will reach 70 degree and hit the "drum" .
+   tone(buzzerPin, frequency('c'), duration_L-p);  //At the same moment, the buzzer will play a note, this is how they get synchronized.   
+   servo1.write(30+k);  //Start getting back to the starting position immediately after hitting the "drum" 
+   delay(280-p);   //Reach the starting position by the end of this delay.
 
   
-  servo1.write(70);    
-  delay(280-p); 
-  tone(buzzerPin, frequency('c'), duration_L-p);          
-
-  servo1.write(30+k);   
-
-  delay(280-p);   
+   servo1.write(70);  //Start moving toward 70 degree.  
+   delay(280-p); //The servo hits the "drum".
+   tone(buzzerPin, frequency('c'), duration_L-p); // The buzzer plays the note.      
+   servo1.write(30+k);  //Start getting back to the starting position immediately after hitting the "drum" 
+   delay(280-p);   //Reach the starting position by the end of this delay.
 
   
-  servo1.write(70);    
-
-  delay(280-p); 
-  tone(buzzerPin, frequency('c'), duration_L-p);          
-
-  servo1.write(40+k);   
-
-  delay(210-p);   
-
-
-    servo1.write(70);    
-
-  delay(210-p);   
-  tone(buzzerPin, frequency('d'), duration_s-p);       
-
-  servo1.write(50+k);   
-
-  delay(140-p);   
-   servo1.write(70);
-   delay(140-p);   
-tone(buzzerPin, frequency('e'), duration_s-p);  
-   
-    servo1.write(30+k);   
-
-  delay(280-p);   
-
-
-  
-  servo1.write(70);    
-
-  delay(280-p); 
-  tone(buzzerPin, frequency('e'), duration_s-p);          
-    servo1.write(40+k);    
-
-  delay(210-p);        
-
+   servo1.write(70);    
+   delay(280-p); 
+   tone(buzzerPin, frequency('c'), duration_L-p);          
+   servo1.write(40+k);   
+   delay(210-p);   
 
 
    servo1.write(70);    
+   delay(210-p);   
+   tone(buzzerPin, frequency('d'), duration_s-p);       
+   servo1.write(50+k);   
+   delay(140-p);   
+         
+       
+   servo1.write(70);
+   delay(140-p);   
+   tone(buzzerPin, frequency('e'), duration_s-p);  
+   servo1.write(30+k);  
+   delay(280-p);   
 
-  delay(210-p);  
-  tone(buzzerPin, frequency('d'), duration_s-p);        
 
-  servo1.write(50+k);   
+   servo1.write(70);    
+   delay(280-p); 
+   tone(buzzerPin, frequency('e'), duration_s-p);          
+   servo1.write(40+k);    
+   delay(210-p);        
 
-  delay(140-p);   
+
+   servo1.write(70);    
+   delay(210-p);  
+   tone(buzzerPin, frequency('d'), duration_s-p);        
+   servo1.write(50+k);   
+   delay(140-p);   
+    
+         
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('e'), duration_s-p);    
-    servo1.write(30+k);   
-    delay(280-p);         
+   servo1.write(30+k);   
+   delay(280-p);         
 
-      servo1.write(70);    
-
-  delay(280-p); 
-  tone(buzzerPin, frequency('f'), duration_s-p);         
-
-  servo1.write(50+k);   
-
-  delay(140-p);   
+         
+   servo1.write(70);    
+   delay(280-p); 
+   tone(buzzerPin, frequency('f'), duration_s-p);         
+   servo1.write(50+k);   
+   delay(140-p);   
+    
+         
    servo1.write(70);
    delay(140-p);   
    tone(buzzerPin, frequency('g'), duration_s-p);  
-    servo1.write(45+k);   
-    delay(280-p);        
-    delay(500-p);
+   servo1.write(45+k);   
+   delay(800-p);        
 
 
 
-
-    servo1.write(70);    
-  delay(203-p); 
-  tone(buzzerPin, frequency('C'), duration_s-p);          
-  servo1.write(50+k);   
-  delay(140-p);   
+         
+   servo1.write(70);    
+   delay(203-p); 
+   tone(buzzerPin, frequency('C'), duration_s-p);          
+   servo1.write(50+k);   
+   delay(140-p);  
+               
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('C'), duration_s-p);    
    servo1.write(50+k);   
-  delay(140-p);   
+   delay(140-p);   
+                 
    servo1.write(70);
    delay(140-p);
    tone(buzzerPin, frequency('C'), duration_s-p);     
-    servo1.write(45+k);   
-    delay(203-p);         
+   servo1.write(45+k);   
+   delay(203-p);         
 
 
 
-    servo1.write(70);    
-  delay(203-p); 
-  tone(buzzerPin, frequency('g'), duration_s-p);          
-  servo1.write(50+k);   
-  delay(140-p);   
+         
+   servo1.write(70);    
+   delay(203-p); 
+   tone(buzzerPin, frequency('g'), duration_s-p);          
+   servo1.write(50+k);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('g'), duration_s-p);    
    servo1.write(50+k);   
-  delay(140-p);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p);
    tone(buzzerPin, frequency('g'), duration_s-p);     
-    servo1.write(45+k);   
-    delay(203-p);         
+   servo1.write(45+k);   
+   delay(203-p);         
 
 
 
-    servo1.write(70);    
-  delay(203-p); 
-  tone(buzzerPin, frequency('e'), duration_s-p);          
-  servo1.write(50+k);   
-  delay(140-p);   
+         
+   servo1.write(70);    
+   delay(203-p); 
+   tone(buzzerPin, frequency('e'), duration_s-p);          
+   servo1.write(50+k);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('e'), duration_s-p);    
    servo1.write(50+k);   
-  delay(140-p);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p);
    tone(buzzerPin, frequency('e'), duration_s-p);     
-    servo1.write(45+k);   
-    delay(203-p);       
+   servo1.write(45+k);   
+   delay(203-p);       
 
 
-     servo1.write(70);    
-  delay(203-p); 
-  tone(buzzerPin, frequency('c'), duration_s-p);          
-  servo1.write(50+k);   
-  delay(140-p);   
+         
+         
+   servo1.write(70);    
+   delay(203-p); 
+   tone(buzzerPin, frequency('c'), duration_s-p);          
+   servo1.write(50+k);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('c'), duration_s-p);    
    servo1.write(50+k);   
-  delay(140-p);   
+   delay(140-p);   
+         
    servo1.write(70);
    delay(140-p);
    tone(buzzerPin, frequency('c'), duration_s-p);     
-    servo1.write(45+k);   
-    delay(203-p);       
+   servo1.write(45+k);   
+   delay(203-p);       
 
 
     
-  servo1.write(70);    
-
-  delay(203-p);  
-  tone(buzzerPin, frequency('g'), duration_s-p);         
-    servo1.write(30+k);    
-
-  delay(280-p);         
-
-
-
+         
+   servo1.write(70);    
+   delay(203-p);  
+   tone(buzzerPin, frequency('g'), duration_s-p);         
+   servo1.write(30+k);    
+   delay(280-p);         
 
 
    servo1.write(70);    
-
-  delay(280-p);        
-tone(buzzerPin, frequency('f'), duration_s-p);   
-  servo1.write(50+k);   
-
-  delay(140-p);   
+   delay(280-p);        
+   tone(buzzerPin, frequency('f'), duration_s-p);   
+   servo1.write(50+k);   
+   delay(140-p);   
+         
+         
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('e'), duration_s-p);     
-    servo1.write(30+k);   
-    delay(280-p);         
+   servo1.write(30+k);   
+   delay(280-p);         
 
    servo1.write(70);    
+   delay(280-p);        
+   tone(buzzerPin, frequency('d'), duration_s-p);   
+   servo1.write(50+k);   
 
-  delay(280-p);        
-tone(buzzerPin, frequency('d'), duration_s-p);   
-  servo1.write(50+k);   
-
-  delay(140-p);   
+         
+   delay(140-p);   
    servo1.write(70);
    delay(140-p); 
    tone(buzzerPin, frequency('c'), duration_s-p);     
-    servo1.write(30+k);   
-    delay(280-p);         
-
-    
-    delay(1000-p);
-
-
-    
+   servo1.write(30+k);   
+         
+  
+    delay(1000);
 
 }
+
+
 
 
 
